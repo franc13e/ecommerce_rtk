f9f07ef91e15715c572893c6b8807f28a5a5de55
Francine <francinefaith03@gmail.com>
2024-12-13 11:13:07 -0500

    Updated shoppingcart.jsx and the full exercise

--- src/Components/ProductList.jsx
+++ src/Components/ProductList.jsx
@@ -1,12 +1,11 @@
-import React from 'react';
-import './ProductList.css'; 
+import React, { useState } from 'react';
 import { useDispatch } from 'react-redux';
-import { useState } from 'react';
-import { addItemtoCart } from './CartSlice';
+import { addItemToCart } from './CartSlice';
+import './ProductList.css'; // Import CSS file for component-specific styles
 
 const ProductList = () => {
-    const dispatch = useDispatch();
-    const [disabledProducts, setDisabledProducts] = useState([]); // State to store disabled products
+  const dispatch = useDispatch();
+  const [disabledProducts, setDisabledProducts] = useState([]); // State to store disabled products
 
   const products = [
     { id: 1, name: 'Product A', price: 60 },
@@ -15,8 +14,8 @@ const ProductList = () => {
   ];
 
   const handleAddToCart = product => {
-    dispatch(addItemtoCart(product));
-    setDisabledProducts([...disabledProducts, products.id]); //Mark product as diabled
+    dispatch(addItemToCart(product));
+    setDisabledProducts([...disabledProducts, product.id]); // Mark the product as disabled
   };
 
   return (
@@ -24,17 +23,17 @@ const ProductList = () => {
       <h2 className="product-list-title">Products</h2>
       <ul className="product-list-items">
         {products.map(product => (
-            <li key={product.id} className="product-list-item">
-                <span>{product.name} - ${product.price}</span>
-                <button
-                    className={`add-to-cart-btn ${disabledProducts.includes(product.id) ? 'disabled' : ''}`}
-                    onClick={() => handleAddToCart(product)}
-                    disabled={disabledProducts.includes(product.id)}> 
-                    Add to Cart
-                </button>
-            </li>
+          <li key={product.id} className="product-list-item">
+            <span>{product.name} - ${product.price}</span>
+            <button 
+              className={`add-to-cart-btn ${disabledProducts.includes(product.id) ? 'disabled' : ''}`} 
+              onClick={() => handleAddToCart(product)}
+              disabled={disabledProducts.includes(product.id)} // Disable button if product is in disabledProducts
+            >
+              Add to Cart
+            </button>
+          </li>
         ))}
-     
       </ul>
     </div>
   );
