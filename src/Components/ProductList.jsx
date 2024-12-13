feab389a28b12681be77f0e912df11f7e217c75e
Francine <francinefaith03@gmail.com>
2024-12-13 10:57:13 -0500

    Update productlist.jsx

--- src/Components/ProductList.jsx
+++ src/Components/ProductList.jsx
@@ -1,13 +1,23 @@
 import React from 'react';
 import './ProductList.css'; 
+import { useDispatch } from 'react-redux';
+import { useState } from 'react';
+import { addItemtoCart } from './CartSlice';
 
 const ProductList = () => {
+    const dispatch = useDispatch();
+    const [disabledProducts, setDisabledProducts] = useState([]); // State to store disabled products
 
   const products = [
     { id: 1, name: 'Product A', price: 60 },
     { id: 2, name: 'Product B', price: 75 },
     { id: 3, name: 'Product C', price: 30 },
   ];
+
+  const handleAddToCart = product => {
+    dispatch(addItemtoCart(product));
+    setDisabledProducts([...disabledProducts, products.id]); //Mark product as diabled
+  };
 
   return (
     <div className="product-list">
@@ -16,7 +26,10 @@ const ProductList = () => {
         {products.map(product => (
             <li key={product.id} className="product-list-item">
                 <span>{product.name} - ${product.price}</span>
-                <button>
+                <button
+                    className={`add-to-cart-btn ${disabledProducts.includes(product.id) ? 'disabled' : ''}`}
+                    onClick={() => handleAddToCart(product)}
+                    disabled={disabledProducts.includes(product.id)}> 
                     Add to Cart
                 </button>
             </li>
